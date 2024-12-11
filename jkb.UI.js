efda035004f0cebcaad4998142998489ca48b389
Matt Jakob <me@mattjakob.com>
2024-12-10 19:21:08 -0500

    fix bug

--- jkb.UI.js
+++ jkb.UI.js
@@ -1,33 +1,37 @@
 import p5 from 'p5';
 
-p5.prototype.createSliderWithLabel = function(min, max, value, step, labelText) {
-  // Create a div to contain the slider and label
-  const container = select('.UIContainer');
+p5.prototype.createSliderWithLabel = function (min, max, value, step, labelText) {
+  // Create or select the UI container
+  let container = select('.UIContainer');
+  if (!container) {
+    container = this.createDiv().class('UIContainer');
+  }
 
-  // Create a label element
-  const label = this.createDiv(`${labelText}: ${slider.value()}`).class('UILabel');
-
-  // Create a slider using the original createSlider function
+  // Create a slider
   const slider = this.createSlider(min, max, value, step).class('UISlider');
 
-  // Add a method to set a random value to the slider
-  container.setRandomValue = function() {
-    let randomValue = random(min, max);
-    if (step >= 1) randomValue = floor(randomValue / step) * step;
-    else if (step > 0) randomValue = round(randomValue / step) * step;
-    slider.value(constrain(randomValue, min, max));
-    label.html(`${labelText}: ${slider.value()}`);
-  };
+  // Create a label
+  const label = this.createDiv(`${labelText}: ${slider.value()}`).class('UILabel');
 
-  // Update the label whenever the slider value changes
+  // Append the slider and label to the container
+  container.child(label);
+  container.child(slider);
+
+  // Update the label when slider value changes
   slider.input(() => {
     label.html(`${labelText}: ${slider.value()}`);
   });
 
-  // Append the label and slider to the container
-  container.child(label);
-  container.child(slider);
+  // Add a method to set a random value to the slider
+  container.setRandomValue = () => {
+    let randomValue = this.random(min, max);
+    if (step >= 1) randomValue = this.floor(randomValue / step) * step;
+    else if (step > 0) randomValue = this.round(randomValue / step) * step;
 
-  // Return the container instead of just the slider
+    slider.value(this.constrain(randomValue, min, max));
+    label.html(`${labelText}: ${slider.value()}`);
+  };
+
+  // Return the container to allow external access to the slider and label
   return container;
 };
